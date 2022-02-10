7bbf5b101c48e2ade80a65f12d8de0ec773467f3
LeAnn Gove <leanngove@hotmail.com>
2022-02-09 22:31:46 -0500

    fixed bug

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,9 +3,9 @@ import React from 'react'
 import './Tile.css'
 
 const Tile = (props) => {
-const color = props.selected || props.matched ? {backgroundColor: color} : null
+const dynamicColor = props.selected || props.matched ? {backgroundColor: props.color} : null
   return (
-    <div className='Tile' style={color}>
+    <div className='Tile' style={dynamicColor}>
     {props.selected || props.matched ? <props.svg /> : null}
     </div>
   )
