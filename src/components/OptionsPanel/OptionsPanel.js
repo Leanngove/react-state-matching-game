eb716fd4ff2f1f95a92e39bddabd172e0590af5d
LeAnn Gove <leanngove@hotmail.com>
2022-02-09 22:27:44 -0500

    hydrating state

--- src/components/OptionsPanel/OptionsPanel.js
+++ src/components/OptionsPanel/OptionsPanel.js
@@ -3,13 +3,14 @@ import React from 'react'
 import Button from '../Button'
 import TileSelector from '../TileSelector'
 
-const OptionsPanel = () => (
+const OptionsPanel = (props) => (
   <div>
-    <TileSelector />
-    <Button />
+    <TileSelector numTiles = {this.props.numTiles} />
+    <Button playing = {this.props.playing}
+    startGame = {this.props.startGame}/>
   </div>
 )
-  
-  
+
+
 
 export default OptionsPanel
